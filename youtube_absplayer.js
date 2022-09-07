faf775acc2d43b378c18c3ba4f6f9656008b41c2
Trần Duy Vương <vuong@Vuongs-MacBook.local>
2022-09-07 21:45:02 +0700

    change

---
+++ youtube_absplayer.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Auto reload absplayer Youtube
+// @namespace    https://github.com/vuongcris4
+// @version      0.1
+// @description  Auto reload absplayer Youtube
+// @author       vuongcris4
+// @match        https://www.youtube.com/watch?*
+// @grant        none
+// ==/UserScript==
+
+window.open("btt://trigger_named/?trigger_name=asbplayer"); //Run better touch tool script
