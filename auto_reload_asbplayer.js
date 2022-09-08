29b19aae3cd8a0307f4085e2517419e1f537b709
Trần Duy Vương <vuong@Vuongs-MacBook.local>
2022-09-08 07:50:47 +0700

    change

--- auto_reload_asbplayer.js
+++ auto_reload_asbplayer.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         Auto reload absplayer Youtube
+// @name         Auto reload absplayer
 // @namespace    https://github.com/vuongcris4
 // @version      0.1
-// @description  Auto reload absplayer Youtube
+// @description  Auto reload absplayer
 // @author       vuongcris4
-// @match        https://www.youtube.com/watch?*
+// @include      https://www.youtube.com/watch?*
+// @include      https://xemphim.vip/watch*
 // @grant        none
 // ==/UserScript==
 
